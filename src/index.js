c66d514096565a09eb83cb492bc9603e0e76e9bd
edogbosunny <sunnyedogbo@gmail.com>
2020-04-10 20:43:53 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,38 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore, combineReducers } from 'redux';
+
+
+const personReducer = (state = {}, actions) => {
+  if (actions.type === 'UPDATE_NAME') {
+    return { name: actions.payload }
+  }
+  // reducre returns our state
+  return state
+}
+
+const gameReducer = (state = {}, actions) => {
+  if (actions.type === 'UPDATE_GAME') {
+    return { name: actions.payload }
+  }
+  return state
+}
+
+const allReducers = combineReducers({ game: gameReducer, person: personReducer })
+
+const initialState = {
+  game: { name: 'football' },
+  person: { name: 'sunny' }
+}
+
+const store = createStore(allReducers, initialState);
+
+console.log('---->', store.getState());
+
+// dispatch helps us to update the state.
+store.dispatch({ type: 'UPDATE_NAME', payload: 'bitcoins' })
+console.log('<-------', store.getState())
 
 ReactDOM.render(
   <React.StrictMode>
